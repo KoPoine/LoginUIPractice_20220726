6d5f2d959ec225c7200c2efa9991ffbc4de53cb0
김민철 <kmc901206@gmail.com>
2022-07-26 11:50:55 +0900

    변수 값 생성 및 값 대입

--- app/src/main/java/com/neppplus/loginuipractice_20220726/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginuipractice_20220726/MainActivity.kt
@@ -12,25 +12,30 @@ class MainActivity : AppCompatActivity() {
 //        MainActivity의 xml파일과 kt파일을 서로 연결해주는 함수(setContentView)
         setContentView(R.layout.activity_main)
 
+//        변수 생성 및 값 대입
+//        val email : String
+//        email = "kmc901206@gmail.com"
+//        변수 생성 및 바로 값 대입 > 변수 값 초기화
+        val email = "kmc901206@gmail.com"
+
+
 //        기초 이벤트 처리 (setOnClickListener -> 클릭 이벤트 처리)
         signUpBtn.setOnClickListener {
 //            버튼이 눌리때마다 실행시켜줄 코드 작성
 //            Toast.makeText(this, "회원가입 버튼 클릭", Toast.LENGTH_SHORT).show()
 //            emailTxt.text = "아이디"
 
-            emailEdt.setText("아이디")
+//            emailEdt.setText("아이디")
         }
 
         loginBtn.setOnClickListener {
             Log.d("로그인버튼", "클릭")
 //            email텍스트뷰의  text속성을 비밀번호 텍스트뷰의 text속성으로 변경
 //            emailTxt.text = passwordTxt.text
-            val email = emailEdt.text
-            val password = passwordEdt.text
+//            val email = emailEdt.text
+//            val password = passwordEdt.text
 
             Toast.makeText(this, email, Toast.LENGTH_SHORT).show()
-
-//            서버에 eamil과 비밀번호 담아둔 변수를 던져서
         }
 
     }
