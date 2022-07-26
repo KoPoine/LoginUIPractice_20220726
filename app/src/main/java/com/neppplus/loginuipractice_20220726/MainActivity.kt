3b52c7692609550ce1754587eacc753fac82cb39
김민철 <kmc901206@gmail.com>
2022-07-26 11:41:29 +0900

    속성 값 조회(get) / 대입 (set) 진행

--- app/src/main/java/com/neppplus/loginuipractice_20220726/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginuipractice_20220726/MainActivity.kt
@@ -15,11 +15,22 @@ class MainActivity : AppCompatActivity() {
 //        기초 이벤트 처리 (setOnClickListener -> 클릭 이벤트 처리)
         signUpBtn.setOnClickListener {
 //            버튼이 눌리때마다 실행시켜줄 코드 작성
-            Toast.makeText(this, "회원가입 버튼 클릭", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "회원가입 버튼 클릭", Toast.LENGTH_SHORT).show()
+//            emailTxt.text = "아이디"
+
+            emailEdt.setText("아이디")
         }
 
         loginBtn.setOnClickListener {
             Log.d("로그인버튼", "클릭")
+//            email텍스트뷰의  text속성을 비밀번호 텍스트뷰의 text속성으로 변경
+//            emailTxt.text = passwordTxt.text
+            val email = emailEdt.text
+            val password = passwordEdt.text
+
+            Toast.makeText(this, email, Toast.LENGTH_SHORT).show()
+
+//            서버에 eamil과 비밀번호 담아둔 변수를 던져서
         }
 
     }
